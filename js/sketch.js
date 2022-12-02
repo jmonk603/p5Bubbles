e66e1bc565f00f3ba92ecb9eeef8285e891043e6
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2022-12-02 11:49:22 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,36 @@
+var x,y,d,hue,a;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+
+    colorMode(HSB);
+    angleMode(DEGREES);
+    a = 0;
+
+    //noStroke();
+    background(255,5);
+
+    for(let i = 0; i < 100; i++){
+        circles();
+    }
 }
 
 function draw() {
-    background(220);
+    push();
+    translate(width/2, height/2);
+    rotate(a);
+    ellipse(0,0,350,150);
+    a += 5;
+    pop();
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function circles() {
+    x = random(width);
+    y = random(height);
+    d = random(50,200);
+    hue = random(120, 255);
+
+    fill(hue, 255, 255, 0.1);
+    circle(x, y, d);
 }
